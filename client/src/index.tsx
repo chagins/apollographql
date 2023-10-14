7d947fbd6f3adc8993836f38396c2342b29fa292
chagins <chagin_yav@hotmail.com>
2023-10-14 18:20:15 +0500

    feat(fe): add apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./apollo";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={apolloClient}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
